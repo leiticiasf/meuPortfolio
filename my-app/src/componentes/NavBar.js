2cbd14ded8824107dfe3c0ad48f112b096e3b7a9
eu <leitica@com>
2024-02-27 22:34:14 -0300

    commit

---
+++ my-app/src/componentes/NavBar.js
@@ -0,0 +1,32 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+
+        <Navbar.Brand href="#home">
+        <img src={''} alt="desenhoDeMim" />
+        </Navbar.Brand>
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#inicio">Início</Nav.Link>
+            <Nav.Link href="#sobreMim">Sobre Mim</Nav.Link>
+            <Nav.Link href="#projetos">Projetos</Nav.Link>
+            <Nav.Link href="#redeSociais">Redes Sociais</Nav.Link>
+          </Nav>
+          <span className='navbar-texto'>
+             <div className='social-icon'>
+              <a href='#'><img src={} alt="" /> </a>
+             </div>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
