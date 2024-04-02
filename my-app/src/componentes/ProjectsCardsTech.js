b7432f8d487824945e9107d726b03edfa8e54d28
eu <leitica@com>
2024-04-02 20:10:06 -0300

    Implementado atividades da área técnica

--- my-app/src/componentes/ProjectsCardsTech.js
+++ my-app/src/componentes/ProjectsCardsTech.js
@@ -1,11 +1,16 @@
 import { Col} from'react-bootstrap';
 
-export const DevCards = ({title, description, imgUrl}) =>{
+export const DevCards = ({title, description, imgUrl, link}) =>{
+    const handleClick = () => {
+        if (link) {
+            window.open(link, '_blank');
+        }
+    };
    
 
     return(
        <Col sm={6} md={4}>
-        <div className='proj-imgbx'>
+        <div className='proj-imgbx' onClick={handleClick} style={{ cursor: 'pointer' }}>
             <img src={imgUrl} />
             <div className='proj-txtx'>
                 <h4>{title}</h4>
@@ -16,3 +21,5 @@ export const DevCards = ({title, description, imgUrl}) =>{
 
     )
 }
+
+            
--- my-app/src/componentes/ProjectsTech.js
+++ my-app/src/componentes/ProjectsTech.js
@@ -1,27 +1,32 @@
  import { Container, Row, Col, Nav, Tab} from "react-bootstrap";
  import { DevCards } from "./ProjectsCardsTech";
+ import fotoCalcReact from "../assets/tecnicoImg/calculadoraReact.png";
+ import fotoCasoUML from "../assets/tecnicoImg/casoDeUso.png";
 
 
 export const DevProjects = () => { /* Parte Desenvolvimento de Sistemas*/
-    const dev = [
+    const ads = [
         {
-            title: "Atividade 1",
-            description: "i somos amigos",
+            title: "Calculadora de Horas",
+            description: "Neste trabalho nós fizemos uma calculadora de horas em React.",
+            imgUrl: fotoCalcReact,
+            link: "https://github.com/leiticiasf/calculadoraReact",
         },
         {
-            title: "Atividade 2",
-            description: "",
+            title: "Postman",
+            description: "Primeiro contato com JAVA e Banco de Dados",
         },
+   
+    ];      
+    
+    const modelagem = [
         {
-            title: "Atividade 3",
-            description: "",
-        },
-        {
-            title: "Atividade 4",
-            description: "",
-        },    
-    ];
-
+            title:"Caso de Uso SA",
+            description: "Esta atividade tinha como requerimento que nós criássemos um diagrama UML que explicasse o funcionamento do nosso site da SA. ",  
+            imgUrl: fotoCasoUML,
+            link: "https://lucid.app/lucidchart/d44af6f0-98d9-4c0b-b85c-38a961aa19c7/edit?viewport_loc=-201%2C399%2C1993%2C789%2C0_0&invitationId=inv_17bc7a94-ccf2-4de0-af71-5722ce18cc03",  
+        }                                                       
+    ]
 
 
 
@@ -36,37 +41,49 @@ return(
             <Tab.Container id="projects-tabs" defaultActiveKey="first">
             <Nav variant="pills" className="nav-pills mb-5 justify-content-center alignt-items-center" id="pills-tab">
                 <Nav.Item>
-                    <Nav.Link eventKey="dev">Desen. de Sistemas</Nav.Link>
+                    <Nav.Link eventKey="ads">Desen. de Sistemas</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="matematica"> Impl. e Manu. de Sistemas </Nav.Link>
+                    <Nav.Link eventKey="ims"> Impl. e Manu. de Sistemas </Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="linguagens" > Modelagem de Sistemas</Nav.Link>
+                    <Nav.Link eventKey="modelagem" > Modelagem de Sistemas</Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                    <Nav.Link eventKey="humanas" > SA </Nav.Link>
+                    <Nav.Link eventKey="sa" > SA </Nav.Link>
                 </Nav.Item>
               
             </Nav>
             <Tab.Content>
-                <Tab.Pane eventKey="dev">
+                <Tab.Pane eventKey="ads">
                    <Row>
                     {
-                         dev.map((dev, index) => {
+                         ads.map((ads, index) => {
                             return(
                                <DevCards
                                 key={index}
-                                {...dev} />
+                                {...ads} />
                             )
                         })
                     }
                    </Row>
                 </Tab.Pane>
 
-             
-           
+                <Tab.Pane eventKey="modelagem">
+                    <Row>
+                        {
+                            modelagem.map((modelagem, index) => {
+                                return(
+                                    <DevCards 
+                                    key={index}
+                                    {...modelagem} />
+                                )
+                            })
+                        }
+                    </Row>
+                </Tab.Pane>
 
+            
             </Tab.Content>
             </Tab.Container>
             </Col>
